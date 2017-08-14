168f17f7356362a75958208140d55fac05716f5b
grzeimann <grzeimann@gmail.com>
2017-08-14 18:40:32 -0400

    added constant sfh

--- sfh.py
+++ sfh.py
@@ -3,12 +3,17 @@
 
 1) Star Formation Histories: 
     a) double power law:
-    b) Noll:
+    b) constant:
 
 .. moduleauthor:: Greg Zeimann <gregz@astro.as.utexas.edu>
 
 """
 
+import numpy as np
+
 def double_powerlaw(t, tau, a, b, c):
     msfr = 10**(a) * ( (t/tau)**b + (t/tau)**(-c))**(-1)
     return msfr 
+    
+def constant(t, a=1.):
+    return a * np.ones(t.shape)
