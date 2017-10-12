ae8fd115851a2abbb49513c45bce761711a08019
grzeimann <grzeimann@gmail.com>
2017-10-12 11:27:16 -0500

    added distance calculation for source

--- mcsed.py
+++ mcsed.py
@@ -18,18 +18,23 @@
 """
 
 import logging
-import numpy as np
 import sfh
 import dust_abs
+import cosmology
+import emcee
+import triangle
+import time
 
-__all__ = ["Mcsed"]
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Mcsed:
     def __init__(self, filter_matrix, ssp_spectra, ssp_ages, ssp_masses,
                  wavelength, sfh_name, dust_abs_name, data_mags=None,
                  data_magerrs=None, redshift=None, filter_flag=None,
-                 input_spectrum=None, input_params=None, sigma_m=0.02):
+                 input_spectrum=None, input_params=None, sigma_m=0.02,
+                 nwalkers=40, nsteps=500):
         ''' Initialize the Mcsed class.
 
         Init
@@ -69,6 +74,10 @@ class Mcsed:
             Fractional error expected from the models.  This is used in
             the log likelihood calculation.  No model is perfect, and this is
             more or less a fixed parameter to encapsulate that.
+        nwalkers : int
+            The number of walkers for emcee when fitting a model
+        nsteps : int
+            The number of steps each walker will make when fitting a model
         '''
         # Initialize all argument inputs
         self.filter_matrix = filter_matrix
@@ -85,6 +94,9 @@ class Mcsed:
         self.input_spectrum = input_spectrum
         self.input_params = input_params
         self.sigma_m = sigma_m
+        self.nwalkers = nwalkers
+        self.nsteps = nsteps
+        self.Dl = cosmology.Cosmology().luminosity_distance(self.redshift)
 
         # Set up logging
         self.setup_logging()
@@ -159,10 +171,11 @@ class Mcsed:
         Returns
         -------
         csp : numpy array (1 dim)
-            Composite stellar population model
+            Composite stellar population model at self.redshift
         mass : float
             Mass for csp given the SFH input
         '''
+        # TODO include D(z)**2 modification to ssp_spectra
         sfh = self.sfh_class.evaluate(self.age_eval)
         ageval = self.sfh_class.age
 
@@ -195,7 +208,8 @@ class Mcsed:
         # Redshift to observed frame
         csp = np.interp(self.wave, self.wave * (1. + self.redshift),
                         spec_dustobscured)
-        return csp, mass
+        # Correct spectra from 10pc to redshift of the source
+        return csp / (4. * np.pi * self.Dl**2), mass
 
     def lnprior(self):
         ''' Simple, uniform prior for input variables
@@ -247,3 +261,121 @@ class Mcsed:
             return lp + lnl, mass
         else:
             return -np.inf, -np.inf
+
+    def get_init_walker_values(self):
+        ''' Before running emcee, this function generates starting points
+        for each walker in the MCMC process.
+
+        Returns
+        -------
+        pos : np.array (2 dim)
+            Two dimensional array with Nwalker x Ndim values
+        '''
+        # We need an initial guess for emcee so we take it from the model class
+        # parameter values and deltas
+        init_params = []
+        init_deltas = []
+        param_classes = ['sfh_class', 'dust_abs_class']
+        for par_cl in param_classes:
+            init_params.append(getattr(self, par_cl).get_params())
+            init_deltas.append(getattr(self, par_cl).get_param_deltas())
+        theta = list(np.hstack(init_params))
+        thetae = list(np.hstack(init_deltas))
+        pos = emcee.utils.sample_ball(theta, thetae, size=self.nwalkers)
+        return pos
+
+    def get_param_names(self):
+        ''' Grab the names of the parameters for plotting
+
+        Returns
+        -------
+        names : list
+            list of all parameter names
+        '''
+        names = []
+        param_classes = ['sfh_class', 'dust_abs_class']
+        for par_cl in param_classes:
+            names.append(getattr(self, par_cl).get_names())
+        names = list(np.hstack(names))
+        return names
+
+    def fit_model(self):
+        ''' Using emcee to find parameter estimations for given set of
+        data magnitudes and errors
+        '''
+        # Need to verify data parameters have been set since this is not
+        # a necessity on initiation
+        self.log.info('Fitting model using emcee')
+        check_vars = ['data_mags', 'data_magerrs', 'redshift', 'filter_flag']
+        for var in check_vars:
+            if getattr(self, var) is None:
+                self.error('The variable %s must be set first' % var)
+
+        pos = self.get_init_walker_values()
+        ndim = pos.shape[1]
+        start = time.time()
+        # Time to set up the sampler and run the mcmc
+        sampler = emcee.EnsembleSampler(self.nwalkers, ndim, self.lnprob,
+                                        a=1.2)
+        initial_steps = 100
+        sampler.run_mcmc(pos, initial_steps)
+        maxprobsearch = sampler.lnprobability == np.max(sampler.lnprobability)
+        indx = np.nonzero(maxprobsearch)[0][0]
+        indy = np.nonzero(maxprobsearch)[1][0]
+        theta = sampler.chain[indx, indy, :]
+        self.set_class_parameters(theta)
+        pos = self.get_init_walker_values()
+
+        # Reset the chain to remove the burn-in samples.
+        sampler.reset()
+
+        # Do real run
+        sampler.run_mcmc(pos, self.nsteps, rstate0=np.random.get_state())
+        end = time.time()
+        elapsed = end - start
+        self.log.info("Total time taken: %0.2f s" % elapsed)
+        self.log.info("Time taken per step: %0.2f ms" %
+                      (elapsed / (initial_steps + self.nsteps)))
+        # Calculate how long the run should last
+        tau = np.max(sampler.acor)
+        burnin_step = np.round(tau*5)
+        self.log.info("Mean acceptance fraction: %0.2f" %
+                      (np.mean(sampler.acceptance_fraction)))
+        self.log.info("AutoCorrelation Steps: %i, Number of Burn-in Steps: %i"
+                      % (np.round(tau), burnin_step))
+        new_chain = np.zeros((self.nwalkers, self.nsteps, ndim+2))
+        new_chain[:, :, :-2] = sampler.chain
+        for i in xrange(len(sampler.blobs)):
+            for j in xrange(len(sampler.blobs[0])):
+                new_chain[j, i, -2] = np.log10(sampler.blobs[i][j])
+        new_chain[:, :, -1] = sampler.lnprobability
+        self.samples = new_chain[:, burnin_step:, :].reshape((-1, ndim+2))
+
+    def triangle_plot(self, outname, lnprobcut=7.5):
+        ''' Make a triangle corner plot for samples from fit
+
+        Input
+        -----
+        outname : string
+            The triangle plot will be saved as "triangle_{outname}.png"
+        lnprobcut : float
+            Some of the emcee chains include outliers.  This value serves as
+            a cut in log probability space with respect to the maximum
+            probability.  For reference, a Gaussian 1-sigma is 2.5 in log prob
+            space.
+        '''
+        # Make selection for three sigma sample
+        chi2sel = (self.samples[:, -1] >
+                   (np.max(self.samples[:, -1], axis=0) - lnprobcut))
+        nsamples = self.samples[chi2sel, :]
+        names = self.get_param_names()
+        names.append('Log Mass')
+        percentilerange = [0.99] * len(names)
+        fig = triangle.corner(nsamples[:, :-1], labels=names,
+                              range=percentilerange, truths=self.input_params,
+                              label_kwargs={"fontsize": 18}, show_titles=True,
+                              title_kwargs={"fontsize": 16},
+                              quantiles=[0.16, 0.5, 0.84], bins=25)
+        fig.set_size_inches(15.0, 15.0)
+        fig.savefig("triangle_%s.png" % (outname), dpi=150)
+        plt.close()
