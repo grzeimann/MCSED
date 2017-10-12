fixed calculation mistake

cosmology.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 class Cosmology:
-    def __init__(self, omega_m=0.31, omega_l=0.69, omega_k=0.00, h0=0.69):
+    def __init__(self, omega_m=0.31, omega_l=0.69, omega_k=0.00, H_0=69):
         ''' Initialize the class
 
         Inputs
@@ -19,14 +19,14 @@ class Cosmology:
             Dark Energy density in the universe today
         omega_k : float
             Curvature density in the universe today
-        h0 : float
+        H_0 : float
             Hubble Constant in the universe today
         '''
         self.omega_m = omega_m
         self.omega_l = omega_l
         self.omega_k = omega_k
-        self.h0 = h0 * 1e5  # km / s / (10 pc)
-        self.c = 2.99792e8  # km / s
+        self.H_0 = H_0  # km / s / (10 pc)
+        self.c = 2.99792e5  # km / s
 
     def luminosity_distance(self, z, stepsize=0.001):
         ''' Calculate the luminosity distance
@@ -43,8 +43,8 @@ class Cosmology:
         d : float
             Luminosity distance (units of 10 pc)
         '''
-        zi = np.arange(0., z + stepsize, stepsize)
+        zi = np.arange(0., z, stepsize)
         E = np.sqrt(self.omega_m * (1 + zi)**3 + self.omega_k * (1 + zi)**2 +
                     self.omega_l)
-        d = (1 + z) * self.c / self.h0 * np.sum(1. / E * stepsize)
-        return d
+        d = (1 + z) * self.c / self.H_0 * np.sum(1. / E * stepsize)
+        return d * 1e5